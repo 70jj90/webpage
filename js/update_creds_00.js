accf225c138848916c3499cfa6025e65f7b128ad
70jj90 <djindji94@gmail.com>
2024-02-09 22:02:38 +0100

    240209_00

--- js/update_creds_00.js
+++ js/update_creds_00.js
@@ -1,3 +1,3 @@
 
 
-document.getElementById("footer_info_creds").innerHTML = "Copyright © 2024 Djindji Ergimen --- Last update: 07/02/2024";
+document.getElementById("footer_info_creds").innerHTML = "Copyright © 2024 Djindji Ergimen --- Last update: 09/02/2024";
