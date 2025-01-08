25ca9db830aa9edac8a27d812323b8b327eca683
70jj90 <djindji94@gmail.com>
2025-01-08 01:43:37 +0100

    buek

--- js/update_creds_00.js
+++ js/update_creds_00.js
@@ -1,3 +1,3 @@
 
 
-document.getElementById("footer_info_creds").innerHTML = "Copyright © 2024 Djindji Ergimen --- Last update: 20/11/2024";
+document.getElementById("footer_info_creds").innerHTML = "Copyright © 2025 Djindji Ergimen --- Last update: 08/01/2025";
